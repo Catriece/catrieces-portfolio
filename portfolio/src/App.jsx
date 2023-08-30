a5776a46c16a6ec16498323881821d901c97c9fd
Catriece Gilbert <119451056+Catriece@users.noreply.github.com>
2023-08-30 13:08:11 -0400

    Updated package.json dependencies

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/home.page.jsx";
-import AboutMe from "./pages/aboutpage/aboutme.page.jsx";
+import HomePage from "./pages/home/home.page.jsx";
+import AboutMe from "./pages/about/about.page.jsx";
 import "./App.css";
 
 function App() {
